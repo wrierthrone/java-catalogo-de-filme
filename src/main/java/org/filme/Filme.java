fed1e99cdf055183568e83bdeb9d9f083edecbb1
Gustavo Kowalschuk <gustavo.kowalschuk@gmail.com>
2024-08-30 16:36:46 -0300

    Código do catalogo de filme

--- src/main/java/org/filme/Filme.java
+++ src/main/java/org/filme/Filme.java
@@ -1,7 +1,24 @@
 package org.filme;
 
+import java.util.Scanner;
+
 public class Filme {
-  int ano = 2024;
-  String nomeFilme = "Quero um carro zero do meu professor";
-  float avaliacao = 1.0f;
+  public static void main(String[] args) {
+    int anoDeLancamento;
+    String nomeFilme;
+    double avaliacao;
+    Scanner ler = new Scanner(System.in);
+
+    System.out.println("Digite seu filme favorito: ");
+    nomeFilme = ler.nextLine();
+    System.out.println("Qual o ano de laçamento? ");
+    anoDeLancamento = ler.nextInt();
+    System.out.println("Diga sua avaliação para o filme: ");
+    avaliacao = ler.nextDouble();
+
+    System.out.println(nomeFilme);
+    System.out.println(anoDeLancamento);
+    System.out.println(avaliacao);
+  }
+
 }
